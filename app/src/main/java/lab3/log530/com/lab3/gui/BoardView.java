d7461e8aa55361061d2fe691056d9bd127067db5
Anthony Piché <piche.anthony@gmail.com>
2014-07-25 14:14:14 -0400

    le board s'adapte à la taille de l'écran.

--- app/src/main/java/lab3/log530/com/lab3/gui/BoardView.java
+++ app/src/main/java/lab3/log530/com/lab3/gui/BoardView.java
@@ -21,7 +21,7 @@ public class BoardView extends SurfaceView implements SurfaceHolder.Callback {
     private int nbCasesHeight = 8;
     private int nbCasesWidth = 8;
 
-    private final static int PIXEL_SIZE = 100;
+    private int pixelSize = 0;
 
     /** Size of a tile in working coordinates. */
     private static final double TILE_SIZE = 200.0;
@@ -101,7 +101,9 @@ public class BoardView extends SurfaceView implements SurfaceHolder.Callback {
     @Override
     public void surfaceCreated(SurfaceHolder holder) {
         Canvas canvas = holder.lockCanvas(null);
+
         canvas.drawColor(Color.WHITE);
+        pixelSize = canvas.getWidth()/8;
 
         Paint paint = new Paint();
         paint.setAntiAlias(true);
@@ -110,11 +112,11 @@ public class BoardView extends SurfaceView implements SurfaceHolder.Callback {
             for(int x = 0 ; x < nbCasesWidth ; x++) {
                 if(((x+1)%2 == 0 && (y+1)%2 != 0) || ((x+1)%2 != 0 && (y+1)%2 == 0)) {
                     paint.setColor(DARK);
-                    canvas.drawRect(x*PIXEL_SIZE, y*PIXEL_SIZE, (x*PIXEL_SIZE)+PIXEL_SIZE, (y*PIXEL_SIZE)+PIXEL_SIZE, paint);
+                    canvas.drawRect(x*pixelSize, y*pixelSize, (x*pixelSize)+pixelSize, (y*pixelSize)+pixelSize, paint);
                 }
                 else {
                     paint.setColor(LIGHT);
-                    canvas.drawRect(x*PIXEL_SIZE, y*PIXEL_SIZE, (x*PIXEL_SIZE)+PIXEL_SIZE, (y*PIXEL_SIZE)+PIXEL_SIZE, paint);
+                    canvas.drawRect(x*pixelSize, y*pixelSize, (x*pixelSize)+pixelSize, (y*pixelSize)+pixelSize, paint);
                 }
             }
         }
